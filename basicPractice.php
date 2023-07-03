c3ada10dec8b023ea208e854a23b39daece192ca
javines24 <javines2403@gmail.com>
2023-07-04 07:23:20 +0800

    challenge

---
+++ basicPractice.php
@@ -0,0 +1,13 @@
+<?php
+  $name = 'Terasa'; // Heading 1
+  $city = 'NYC';
+  $movie = 'Star Wars'; //Italic
+  $friend = 'George';  // Bold
+  $candy = 'Sour Patch Kids';
+
+  echo "<h1> $name </h1>";
+  echo "<p> $city </p>";
+  echo "<i> $movie </i> <br/>";
+  echo "<b> $friend </b>";
+  echo "<p> $candy </p>"
+?>
