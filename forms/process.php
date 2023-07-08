4a614e8f3f7b8c42246aae3d40fa4fc2cc6c2733
James Vincent Escartin <javines2403@gmail.com>
2023-07-08 09:21:42 +0800

    sending mail

--- forms/process.php
+++ forms/process.php
@@ -1,16 +1,42 @@
 <?php
  
- if(! empty( $_POST )){
-  foreach( $_POST as &$value) {
-   // $value = trim($value);
-  }
+ /**
+  * Data Sanitation
+  */
+//  if(! empty( $_POST )){
+//   foreach( $_POST as &$value) {
+//    // $value = trim($value);
+//   }
 
-  echo $_POST['email'] . ' ' . $_POST['message'];
+//   echo $_POST['email'] . ' ' . $_POST['message'];
 
-  $_POST['email'] = filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL);
-  $_POST['message'] = filter_var( $_POST['message'], FILTER_SANITIZE_ADD_SLASHES);
+//   $_POST['email'] = filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL);
+//   $_POST['message'] = filter_var( $_POST['message'], FILTER_SANITIZE_ADD_SLASHES);
 
-  echo "<br/>" . $_POST['email'];
-  echo "<br/>" . $_POST['message'];
+//   echo "<br/>" . $_POST['email'];
+//   echo "<br/>" . $_POST['message'];
   
- }
+//  }
+
+/**
+ * Sending Email Practice
+ */
+$message = '';
+foreach ($_POST as $key => $value){
+  if(is_array($value)){
+    $value = implode(', ', $value);
+  }
+  $message .= ucfirst($key) . "is $value \n\n";
+}
+
+$to = "James Vincent Castillanes Escartin<javines@gmail.com>";
+$subject = "Hello A Practice";
+$from = $_POST['firstName'] . "<" . $_POST['email'] . ">";
+
+$headers = 'From: ' . $from . "\r\n" .
+'Reply-To: ' . $from . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers)){
+  echo "<h3>Your Email has been sent</h3>";
+}
